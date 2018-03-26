a9265b54548dc7b3cc0a80d6d7e9c13ce7077372
Sylvestris <34038694+Sylvestris@users.noreply.github.com>
2018-03-26 18:07:23 +0200

    Add files via upload

---
+++ WykrywaczZnakow/WykrywaczZnakow/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WykrywaczZnakow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zdjecieGlowneBox = new System.Windows.Forms.PictureBox();
+            this.zdjecieCannyBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_wczytajZdjecie = new System.Windows.Forms.Button();
+            this.btn_wykryjZnaki = new System.Windows.Forms.Button();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.zdjecieWykrytyZnak = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.zdjecieGlowneBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zdjecieCannyBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zdjecieWykrytyZnak)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zdjecieGlowneBox
+            // 
+            this.zdjecieGlowneBox.Location = new System.Drawing.Point(12, 73);
+            this.zdjecieGlowneBox.Name = "zdjecieGlowneBox";
+            this.zdjecieGlowneBox.Size = new System.Drawing.Size(450, 320);
+            this.zdjecieGlowneBox.TabIndex = 1;
+            this.zdjecieGlowneBox.TabStop = false;
+            this.zdjecieGlowneBox.Click += new System.EventHandler(this.zdjecieGlowneBox_Click);
+            // 
+            // zdjecieCannyBox
+            // 
+            this.zdjecieCannyBox.Location = new System.Drawing.Point(488, 73);
+            this.zdjecieCannyBox.Name = "zdjecieCannyBox";
+            this.zdjecieCannyBox.Size = new System.Drawing.Size(250, 150);
+            this.zdjecieCannyBox.TabIndex = 2;
+            this.zdjecieCannyBox.TabStop = false;
+            this.zdjecieCannyBox.Click += new System.EventHandler(this.zdjecieCannyBox_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(762, 73);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(450, 320);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_wczytajZdjecie
+            // 
+            this.btn_wczytajZdjecie.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_wczytajZdjecie.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_wczytajZdjecie.Location = new System.Drawing.Point(12, 27);
+            this.btn_wczytajZdjecie.Name = "btn_wczytajZdjecie";
+            this.btn_wczytajZdjecie.Size = new System.Drawing.Size(131, 40);
+            this.btn_wczytajZdjecie.TabIndex = 5;
+            this.btn_wczytajZdjecie.Text = "Otwórz";
+            this.btn_wczytajZdjecie.UseVisualStyleBackColor = true;
+            this.btn_wczytajZdjecie.Click += new System.EventHandler(this.btn_wczytajZdjecie_Click);
+            // 
+            // btn_wykryjZnaki
+            // 
+            this.btn_wykryjZnaki.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_wykryjZnaki.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btn_wykryjZnaki.Location = new System.Drawing.Point(149, 27);
+            this.btn_wykryjZnaki.Name = "btn_wykryjZnaki";
+            this.btn_wykryjZnaki.Size = new System.Drawing.Size(147, 40);
+            this.btn_wykryjZnaki.TabIndex = 6;
+            this.btn_wykryjZnaki.Text = "Rozpocznij";
+            this.btn_wykryjZnaki.UseVisualStyleBackColor = true;
+            this.btn_wykryjZnaki.Click += new System.EventHandler(this.btn_wykryjZnaki_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(12, 409);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(1212, 23);
+            this.progressBar2.TabIndex = 8;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // zdjecieWykrytyZnak
+            // 
+            this.zdjecieWykrytyZnak.Location = new System.Drawing.Point(488, 243);
+            this.zdjecieWykrytyZnak.Name = "zdjecieWykrytyZnak";
+            this.zdjecieWykrytyZnak.Size = new System.Drawing.Size(250, 150);
+            this.zdjecieWykrytyZnak.TabIndex = 9;
+            this.zdjecieWykrytyZnak.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 459);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 34);
+            this.label1.TabIndex = 10;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1234, 559);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zdjecieWykrytyZnak);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.btn_wykryjZnaki);
+            this.Controls.Add(this.btn_wczytajZdjecie);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.zdjecieCannyBox);
+            this.Controls.Add(this.zdjecieGlowneBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.zdjecieGlowneBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zdjecieCannyBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zdjecieWykrytyZnak)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox zdjecieGlowneBox;
+        private System.Windows.Forms.PictureBox zdjecieCannyBox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_wczytajZdjecie;
+        private System.Windows.Forms.Button btn_wykryjZnaki;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.PictureBox zdjecieWykrytyZnak;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WykrywaczZnakow/WykrywaczZnakow/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace WykrywaczZnakow
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> imgInput;
+        Image<Bgr, byte> imgSmooth;
+        List<PictureBox> wykryteZnaki = new List<PictureBox>();
+
+        int pozycja_wykrytegoZnaku_x = 1114;
+        int licznik_wykrytych_znakow = 0;
+        bool isZnak_1 = false;
+        bool isZnak_2 = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(484, 442);
+            progressBar2.Maximum = 10;
+
+        }
+
+        public void wyczysc_program()
+        {
+            zdjecieGlowneBox.Image = null;
+            zdjecieCannyBox.Image = null;
+            zdjecieWykrytyZnak.Image = null;
+            pictureBox2.Image = null;
+            this.Size = new Size(484, 442);
+            resetujProgressBar();
+
+            //resetowanie tablicy wykrytych znakow
+            foreach(PictureBox znak in wykryteZnaki)
+            {
+                znak.Image = null;
+                this.Controls.Remove(znak);
+                
+            }
+            wykryteZnaki.Clear();
+
+            licznik_wykrytych_znakow = 0;
+            isZnak_1 = false;
+            isZnak_2 = false;
+            pozycja_wykrytegoZnaku_x = 1114;
+            label1.Text = "";
+        }
+
+        private void zwiekszProgressBar(int liczba)
+        {
+            progressBar2.Increment(liczba);
+        }
+
+        public void resetujProgressBar()
+        {
+            progressBar2.Value = 0;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rodzaj_zdjecia"></param>
+        private void ustawWykrytyZnak(int rodzaj_zdjecia)
+        {
+            String nazwa = "";
+            Image newImage = Image.FromFile(@"C:\Users\Sylwek\Desktop\Informatyka\IV\Zaawansowana Grafika Użytkowa\ZGU - projekt zaliczeniowy\img\d1_2.png");
+            const int staly_mnoznik_x = 125;
+
+            if (rodzaj_zdjecia == 1 && !isZnak_1)
+            {
+                pozycja_wykrytegoZnaku_x = pozycja_wykrytegoZnaku_x - (staly_mnoznik_x * licznik_wykrytych_znakow);
+                licznik_wykrytych_znakow++;
+
+                nazwa = "D-1";
+                newImage = Image.FromFile(@"C:\Users\Sylwek\Desktop\Informatyka\IV\Zaawansowana Grafika Użytkowa\ZGU - projekt zaliczeniowy\img\d1_2.png");
+                isZnak_1 = true;
+            }
+
+            if (rodzaj_zdjecia == 2 && !isZnak_2)
+            {
+                pozycja_wykrytegoZnaku_x = pozycja_wykrytegoZnaku_x - (staly_mnoznik_x * licznik_wykrytych_znakow);
+                licznik_wykrytych_znakow++;
+
+                nazwa = "A-7";
+                newImage = Image.FromFile(@"C:\Users\Sylwek\Desktop\Informatyka\IV\Zaawansowana Grafika Użytkowa\ZGU - projekt zaliczeniowy\img\Znak_A-7.jpg");
+                isZnak_2 = true;
+            }
+
+            if (nazwa == "")
+                return;
+
+            PictureBox picture = new PictureBox
+            {
+                Name = nazwa,
+                Size = new Size(110, 110),
+                Location = new Point(pozycja_wykrytegoZnaku_x, 438),
+                Image = newImage,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            wykryteZnaki.Add(picture);
+        }
+
+        private void wykryjZnaki()
+        {
+            //lista trójkątów
+            List<Triangle2DF> triangleList = new List<Triangle2DF>();
+
+            //lista prostokątów i kwadratów
+            List<RotatedRect> boxList = new List<RotatedRect>();
+
+            zwiekszProgressBar(1);
+
+            Image<Gray, byte> canny_zdj = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray(0));
+            canny_zdj = imgInput.Canny(300, 250);
+
+            zdjecieCannyBox.Image = canny_zdj.Bitmap;
+            zdjecieCannyBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            zwiekszProgressBar(2);
+
+            LineSegment2D[] lines = CvInvoke.HoughLinesP(
+               canny_zdj,
+               1,
+               Math.PI / 45.0,
+               20, 
+               30, 
+               10); 
+
+            Image<Gray, byte> imgOut = canny_zdj.Convert<Gray, byte>().ThresholdBinary(new Gray(50), new Gray(200));
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat hier = new Mat();
+
+            zwiekszProgressBar(1);
+
+            imgSmooth = imgInput.PyrDown().PyrUp();
+            imgSmooth._SmoothGaussian(3);
+            imgOut = imgSmooth.InRange(new Bgr(0, 140, 150), new Bgr(80, 255, 255));
+            imgOut = imgOut.PyrDown().PyrUp();
+            imgOut._SmoothGaussian(3);
+
+            zwiekszProgressBar(2);
+
+            Dictionary<int, double> dict = new Dictionary<int, double>();
+            CvInvoke.FindContours(imgOut, contours, null, Emgu.CV.CvEnum.RetrType.List, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            label1.Text = contours.Size.ToString();
+            if (contours.Size > 0)
+            {
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 50) 
+                        {
+                            if (approxContour.Size == 3)
+                            {
+                                Point[] pts = approxContour.ToArray();
+                                triangleList.Add(new Triangle2DF(
+                                   pts[0],
+                                   pts[1],
+                                   pts[2]
+                                   ));
+
+                                if (pts[1].X > pts[0].X && pts[1].Y > pts[0].Y)
+                                {
+                                    ustawWykrytyZnak(2);
+                                    double area = CvInvoke.ContourArea(contours[i]);
+                                    dict.Add(i, area);
+                                }
+                            }
+                            else if (approxContour.Size == 4)
+                            {
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                       edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                if (isRectangle)
+                                {
+                                    RotatedRect rrect = CvInvoke.MinAreaRect(contours[i]);
+                                    if ((rrect.Angle < -40 && rrect.Angle > -50)|| (rrect.Angle > 40 && rrect.Angle < 50))
+                                    {
+                                        boxList.Add(CvInvoke.MinAreaRect(approxContour));
+                                        double area = CvInvoke.ContourArea(contours[i]);
+                                        dict.Add(i, area);
+                                        ustawWykrytyZnak(1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            zwiekszProgressBar(2);
+
+            var item = dict.OrderByDescending(v => v.Value);
+
+            foreach (var it in item)
+            {
+                int key = int.Parse(it.Key.ToString());
+
+                Rectangle rect = CvInvoke.BoundingRectangle(contours[key]);
+                CvInvoke.Rectangle(imgInput, rect, new MCvScalar(0, 0, 255), 1);
+            }
+
+            zwiekszProgressBar(2);
+            pictureBox2.Image = imgInput.Bitmap;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            Image<Bgr, Byte> lineImage = imgInput.CopyBlank();
+            foreach (LineSegment2D line in lines)
+            lineImage.Draw(line, new Bgr(Color.Red), 1);
+            zdjecieWykrytyZnak.Image = lineImage.Bitmap;
+            zdjecieWykrytyZnak.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog okno_pliku = new OpenFileDialog();
+
+            if(okno_pliku.ShowDialog() == DialogResult.OK)
+            {
+                imgInput = new Image<Bgr, byte>(okno_pliku.FileName);
+                zdjecieGlowneBox.Image = imgInput.Bitmap;
+            }
+        }
+
+        private void plikToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zdjecieCannyBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zdjecieGlowneBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_wczytajZdjecie_Click(object sender, EventArgs e)
+        {
+            wyczysc_program();
+            OpenFileDialog okno_pliku = new OpenFileDialog();
+
+            if (okno_pliku.ShowDialog() == DialogResult.OK)
+            {
+                imgInput = new Image<Bgr, byte>(okno_pliku.FileName);
+                zdjecieGlowneBox.Image = imgInput.Bitmap;
+            }
+            zdjecieGlowneBox.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        public void wyswietlWykryteZnaki()
+        {
+            label1.Text = "Wykryto znaki: ";
+            string spacja;
+
+            if (wykryteZnaki.Count < 1)
+                spacja = " ";
+            else
+                spacja = ", ";
+
+            int licznik = 0;
+            foreach (PictureBox znak in wykryteZnaki)
+            {
+                licznik++;
+                this.Controls.Add(znak);
+                if (licznik == wykryteZnaki.Count)
+                    spacja = "";
+                label1.Text += znak.Name + spacja;
+            }
+        }
+
+        private void btn_wykryjZnaki_Click(object sender, EventArgs e)
+        {
+            if (imgInput != null)
+            {
+                this.Size = new Size(1250, 600);
+                wykryjZnaki();
+                wyswietlWykryteZnaki();
+            }
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
